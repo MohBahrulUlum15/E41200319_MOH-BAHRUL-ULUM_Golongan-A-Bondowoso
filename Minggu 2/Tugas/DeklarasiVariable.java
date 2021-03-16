c8a92ef088315b1ce4fdf77ccff7147cd3866776
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-03-16 21:33:58 +0700

    Create DeklarasiVariable.java

---
+++ Minggu 2/Tugas/DeklarasiVariable.java
@@ -0,0 +1,21 @@
+package com.belajarkoding;
+
+public class DeklarasiVariable {
+
+    public static void main(String[] args) {
+        
+        //Deklarasi variable yang benar
+        boolean gameOver = false;
+        int students=50,classes=3;
+        double sales_tax;
+        short number1;
+        
+        //Deklarasi variable yang salah
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+                
+    }
+    
+}
