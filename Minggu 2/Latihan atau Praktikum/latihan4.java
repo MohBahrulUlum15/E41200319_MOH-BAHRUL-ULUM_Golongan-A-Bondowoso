3fdfa9d539c10df4b0f5ec7878c99bafa9e146b3
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-03-16 09:21:45 +0700

    Create latihan4.java

---
+++ Minggu 2/Latihan atau Praktikum/latihan4.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.belajarkoding.workshop;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author My Computer
+ */
+public class latihan4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner s1 = new Scanner(System.in);
+        
+        System.out.println("Masukkan nilai 1 = ");
+        int nilai1 = s1.nextInt();
+        System.out.println("Masukkan nilai 2 = ");
+        int nilai2 = s1.nextInt();
+        
+        int jumlah = nilai1 + nilai2;
+        System.out.println("Jumlah = " + jumlah);
+    }
+    
+}
