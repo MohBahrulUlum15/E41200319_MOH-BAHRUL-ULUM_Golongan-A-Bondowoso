ead784b1b804e1d6407135dc3dc8c8e16f2bc5df
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-03-23 20:31:42 +0700

    Create Tugas1.java

---
+++ Minggu 3/Tugas/Tugas1.java
@@ -0,0 +1,14 @@
+package minggu3;
+
+public class Tugas1 {
+
+    //-Membuat array yang berisi nama lengkap
+
+    public static void main(String[] args) {
+        String[] nama = {"Moh.", "Bahrul", "'Ulum"};
+        for (int i = 0; i < nama.length; i++){
+            System.out.print(nama[i]);
+            System.out.print(" ");
+        }
+    }
+}
