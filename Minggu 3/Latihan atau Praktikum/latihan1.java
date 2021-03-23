5c1117733513b82752596c11655cdaefa4ebed49
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-03-23 20:24:18 +0700

    Create latihan1.java

---
+++ Minggu 3/Latihan atau Praktikum/latihan1.java
@@ -0,0 +1,10 @@
+package minggu3;
+
+public class latihan1 {
+    
+    public static void main(String[] args) {
+        int[] angka = {3, 7, 8, 1, 10, 20, 35};
+        System.out.println("Jumlah elemen array angka = " + angka.length);
+    }
+    
+}
