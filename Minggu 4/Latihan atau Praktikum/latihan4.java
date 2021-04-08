17e5f5cbedcaa998c35b3818e46facd85345ca51
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-09 00:58:30 +0700

    Create latihan4.java

---
+++ Minggu 4/Latihan atau Praktikum/latihan4.java
@@ -0,0 +1,17 @@
+package minggu4;
+
+public class latihan4 {
+
+    //+++++ Statement -- for - each +++++//
+
+    public static void main(String[] args) {
+
+        //membuat array
+        int angka[] = {3, 1, 42, 24, 12};
+
+        //menggunakan perulangan for-each untuk menampilkan angka
+        for (int x : angka) {
+            System.out.print(x + " ");
+        }
+    }
+}
