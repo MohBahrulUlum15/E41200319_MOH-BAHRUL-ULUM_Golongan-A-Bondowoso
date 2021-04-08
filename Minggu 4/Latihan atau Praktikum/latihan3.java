986794cc2a68120e193dc8183cdd23927e99671f
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-09 00:58:21 +0700

    Create latihan3.java

---
+++ Minggu 4/Latihan atau Praktikum/latihan3.java
@@ -0,0 +1,16 @@
+package minggu4;
+
+public class latihan3 {
+
+    //+++++ Statement -- for +++++//
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
