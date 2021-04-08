afad822142cb4639eae881c90003ee7751ba4a59
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-09 00:58:03 +0700

    Create latihan1.java

---
+++ Minggu 4/Latihan atau Praktikum/latihan1.java
@@ -0,0 +1,17 @@
+package minggu4;
+
+public class latihan1 {
+
+    //+++++ Statement -- while +++++//
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i <= 10) {
+            //blok kode yang akan diulang
+            System.out.println("Perulangan ke-" + i);
+            //increment nilai i
+            i++;
+        }
+    }
+}
