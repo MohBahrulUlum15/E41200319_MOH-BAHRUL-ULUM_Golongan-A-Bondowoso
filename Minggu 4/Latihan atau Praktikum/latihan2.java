da00bb24555cbf3bdedee54d40d383f85be5c4fc
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-09 00:58:10 +0700

    Create latihan2.java

---
+++ Minggu 4/Latihan atau Praktikum/latihan2.java
@@ -0,0 +1,16 @@
+package minggu4;
+
+public class latihan2 {
+
+    //+++++ Statement -- do - while +++++//
+
+    public static void main(String[] args) {
+        //membuat variabel
+        int i = 0;
+
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        } while (i <= 10);
+    }
+}
