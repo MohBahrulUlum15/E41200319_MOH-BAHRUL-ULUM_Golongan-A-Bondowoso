29d534d9df482b511cf4193731f64606d788c5c6
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-25 19:23:56 +0700

    Create Pelajar.java

---
+++ Minggu 7/Latihan atau Praktikum/Pewarisan/Pelajar.java
@@ -0,0 +1,21 @@
+package Pewarisan;
+
+/**
+ * @author MOH. BAHRUL 'ULUM
+ */
+public class Pelajar extends Orang{
+    private String nim;
+    private String asalSekolah;
+    private double nilai;   //  range : 0-30
+    
+    public Pelajar(String nama, double tinggi, double berat, String nim, String sekolah, double nilai) {
+//        super(nama, tinggi, berat);
+        this.nim = nim;
+        this.asalSekolah = sekolah;
+        this.nilai = nilai;
+    }
+    
+    public String toString() {
+        return (super.toString()+ "\nNIM : " + nim + "\nSekolah : " + asalSekolah + "\nNilai : " + nilai);
+    }
+}
