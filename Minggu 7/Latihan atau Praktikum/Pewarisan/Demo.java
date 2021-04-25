2c686d610c1db388f0eb3976c17a22da618b869b
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-25 19:23:42 +0700

    Create Demo.java

---
+++ Minggu 7/Latihan atau Praktikum/Pewarisan/Demo.java
@@ -0,0 +1,25 @@
+package Pewarisan;
+
+/**
+ * @author MOH. BAHRUL 'ULUM
+ */
+public class Demo {
+    public static void main(String[] args) {
+        A superOb = new A();
+        B subOb = new B();
+        
+        System.out.println("SuperClass");
+        superOb.x = 10;
+        superOb.y = 20;
+        superOb.TampilkanNilaiXY();
+        
+        System.out.println("SubClass");
+        subOb.x = 5;
+        subOb.y = 4;
+        subOb.TampilkanNilaiXY();
+        
+        //  member tambahan yang hanya ada pada subclass
+        subOb.z = 50;
+        subOb.TampilkanJumlah();
+    }
+}
