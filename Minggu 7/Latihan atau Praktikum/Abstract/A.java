4de18de93695d3d94894819fbd9fc8607891921a
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-25 19:22:13 +0700

    Create A.java

---
+++ Minggu 7/Latihan atau Praktikum/Abstract/A.java
@@ -0,0 +1,12 @@
+package Abstract;
+
+/**
+ * @author MOH. BAHRUL 'ULUM
+ */
+public abstract class A {   // Deklarasi class
+    abstract public void method2();     // Deklarasi method abstract
+    // method konkrit yang memiliki implementasi
+    public void method1(){
+        System.out.println("Method konkrit dari class A");
+    }
+}
