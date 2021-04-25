a30110808b7b97a998734bc043aaeb184523220a
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-25 19:26:20 +0700

    Create User.java

---
+++ Minggu 7/Tugas/tugasTambahan/User.java
@@ -0,0 +1,31 @@
+package tugasTambahan;
+
+/**
+ *
+ * @author Moh. Bahrul 'Ulum
+ */
+
+public class User {
+    public static void main(String[] args) {
+        Televisi tv = new Televisi("TV Tempoe Doeloe, 14 inches", 10);
+        String[] semuaChannel = {"RCTI", "SCTV", "INDOSIAR", "LA TV",
+                                "METRO TV", "TRANS TV", "TPI", "TV 7",
+                                "TVRI", "TV G", "AN TV"
+                                };
+        String[] channelFavorit = {"RCTI", "SCTV", "INDOSIAR", "LA TV",
+                                "METRO TV", "TRANS TV", "TPI", "TV 7"
+                                };
+        
+        System.out.println("Pak Lukman, membeli Televisi : " + tv.getDeskripsi());
+        
+        tv.getChannels();
+        tv.setChannels(semuaChannel);
+        tv.setChannels(channelFavorit);
+        
+        tv.setChannelAktif(3);
+        tv.setVolume(10);
+        tv.setChannelAktif(10);
+        tv.setChannelAktif(1);
+        
+    }
+}
