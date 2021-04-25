1034e4e77ffb8faff3c7d7d444e75923dc77a6f5
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-25 19:22:59 +0700

    Create A.java

---
+++ Minggu 7/Latihan atau Praktikum/Overriding/A.java
@@ -0,0 +1,10 @@
+package Overriding;
+
+/**
+ * @author MOH. BAHRUL 'ULUM
+ */
+public class A {
+    public void tampilkanKeLayar(){
+        System.out.println("Method milik class A dipanggil");
+    }
+}
