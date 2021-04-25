aa74a0c2d5f855b0983f2244e04ec16c9b4013ba
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-25 19:22:32 +0700

    Create A.java

---
+++ Minggu 7/Latihan atau Praktikum/Konstruktor/A.java
@@ -0,0 +1,15 @@
+package Konstruktor;
+
+/**
+ * @author MOH. BAHRUL 'ULUM
+ */
+public class A {
+
+    A() {   //  super(); Tambahan kode secara otomatis oleh Java untuk memanggil Konstruktor dari SuperClassnya
+        
+        super();    //  Konstruktor deault dari class 
+                    //  Object dipanggil
+        System.out.println("Konstruktor class A dieksekusi ...");
+    }
+  
+}
