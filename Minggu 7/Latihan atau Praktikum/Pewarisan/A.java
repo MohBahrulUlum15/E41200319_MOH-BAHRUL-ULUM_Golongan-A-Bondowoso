6eb6fbd3e5756c009b7bc8b719078d67e5892206
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-25 19:23:29 +0700

    Create A.java

---
+++ Minggu 7/Latihan atau Praktikum/Pewarisan/A.java
@@ -0,0 +1,13 @@
+package Pewarisan;
+
+/**
+ * @author MOH. BAHRUL 'ULUM
+ */
+public class A {
+    int x;
+    int y;
+    
+    void TampilkanNilaiXY(){
+        System.out.println("Nilai x : " + x + ", y : " + y);
+    }
+}
