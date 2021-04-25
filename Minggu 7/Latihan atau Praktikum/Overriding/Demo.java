525c52b7598831ca8ed064df5296915854a008a0
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-25 19:23:24 +0700

    Create Demo.java

---
+++ Minggu 7/Latihan atau Praktikum/Overriding/Demo.java
@@ -0,0 +1,11 @@
+package Overriding;
+
+/**
+ * @author MOH. BAHRUL 'ULUM
+ */
+public class Demo {
+    public static void main(String[] args) {
+        B subOb = new B();
+        subOb.tampilkanKeLayar();
+    }
+}
