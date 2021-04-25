e2b09f8dba396c912602840afeb76195f6e66f16
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-25 19:22:19 +0700

    Create B.java

---
+++ Minggu 7/Latihan atau Praktikum/Abstract/B.java
@@ -0,0 +1,10 @@
+package Abstract;
+
+/**
+ * @author MOH. BAHRUL 'ULUM
+ */
+public class B extends A{
+    public void method2(){      // Method abstract di-override dan dibuat
+        System.out.println("Method abstract yang sudah menjadi konkrit dalam class B");
+    }
+}
