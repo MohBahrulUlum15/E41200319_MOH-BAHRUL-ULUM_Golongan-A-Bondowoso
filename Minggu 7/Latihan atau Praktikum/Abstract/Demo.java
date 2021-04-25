532da3c0ea2c2441cdfaaa5f3de19356543c578c
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2021-04-25 19:22:25 +0700

    Create Demo.java

---
+++ Minggu 7/Latihan atau Praktikum/Abstract/Demo.java
@@ -0,0 +1,12 @@
+package Abstract;
+
+/**
+ * @author MOH. BAHRUL 'ULUM
+ */
+public class Demo {
+    public static void main(String[] args) {
+        B ob = new B();
+        ob.method1();
+        ob.method2();
+    }
+}
